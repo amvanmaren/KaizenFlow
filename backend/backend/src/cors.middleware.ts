b62434510c9c865f1907109ef719d369d9df38a6
amvanmaren <vanmaren@gmail.com>
2025-12-16 16:46:55 -0800

    Added cors package in order to allow internal requests on the system.  Currently not working.  Still blocked by No 'Access-Control-Allow-Origin' when attempting to login from frontend.

---
+++ backend/backend/src/cors.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const allowedOrigin = 'http://localhost:5173'; // Frontend origin
+    res.header('Access-Control-Allow-Origin', allowedOrigin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('credentials', 'true');
+    next(); 
+  }
+}
--- backend/backend/src/main.ts
+++ backend/backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsMiddleware } from './cors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors(CorsMiddleware)
+  // app.use(CorsMiddleware);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Specify allowed methods
+    credentials: true, // If you need to handle cookies or authorization headers
+    allowedHeaders: 'Content-Type, Authorization',
+    
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
